44738c1af346951b2bc5bf1652de5eb1abe6f69f
Jarvis <39226019+Jarfle8411@users.noreply.github.com>
2019-01-26 17:48:01 -0500

    Add files via upload
    I created a simple do while loop which will continue executing until the user types q into the console.

---
+++ Do While Loop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Array_Sort_Method
+{
+    class simple_array_sort_method
+    {
+        static void Main(string[] args)
+        {
+            // Do_While construct is do{}while(test)
+            string x;
+            do
+            { // q means to quit
+
+                Console.WriteLine("Type an \"q\" to end program");
+                Console.WriteLine("enter response:");
+                x = Console.ReadLine();
+                Console.WriteLine(x);
+            }
+            while (x != "q");
+        }
+    }
+}
